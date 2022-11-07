a92aa490dc71baa85d7e35979b5ce7c4cb5252f5
Matute12 <mateohofb@gmail.com>
2022-11-07 16:21:32 -0300

    miperfil

--- toochat/src/screens/Profile/Profile.js
+++ toochat/src/screens/Profile/Profile.js
@@ -1,14 +1,26 @@
-import { Text, View, TouchableOpacity} from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet} from 'react-native'
 import React, { Component } from 'react'
-import {auth} from '../../firebase/config'
+import {db, auth} from '../../firebase/config'
 
 class Profile extends Component {
   
   constructor(){
     super()
     this.state={
-      
+      misDatos: {},
+      id:''
     }
+  }
+
+  componentDidMount(){
+    db.collection('users')
+    .where('email', '==', auth.currentUser.email)
+    .onSnapshot(doc => {
+      doc.forEach(doc => this.setState({
+        id: doc.id,
+        misDatos: doc.data()
+      })) 
+    })
   }
 
   cerrarSesion(){
@@ -17,17 +29,31 @@ class Profile extends Component {
     .catch(err => console.log(err))
   } 
 
+
+
   render() {
     return (
       <View>
-        <Text>Profile</Text>
-
+        <Text style={styles.text}>Email: {this.state.misDatos.email}</Text>
+        <Text style={styles.text}>Usuario: {this.state.misDatos.usuario}</Text>
+        <Text style={styles.text}>Biografia: {this.state.misDatos.biografia}</Text>
         <TouchableOpacity onPress={()=> this.cerrarSesion()}>
           <Text>Cerrar sesión</Text>
         </TouchableOpacity>
       </View>
     )
-  }
+  } 
 }
+
+const styles = StyleSheet.create({
+  text:{
+    textAlign: 'center',
+    fontSize: 24,
+    marginBottom: 15,
+    fontWeight: 'bold',
+    color: '#0095F6',
+  }
+})
+
 
 export default Profile
